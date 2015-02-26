947ab7833f459411a5ca83cfd406d7a02dfdf355
indosandi <indosandi@gmail.com>
2015-02-25 23:46:15 -0600

    mnst.py

--- mnst.py
+++ mnst.py
@@ -19,12 +19,6 @@ if __name__ == "__main__":
         del ptest
         
         print(test.shape)
-        #noeig=200
-        #pca=PCA(n_components=noeig)
-        #pca.fit(train)
-        #traindata=pca.fit_transform(train)
-        #print(target.shape)
-        #print(traindata.shape)
         noTree=100
         critF='entropy'
         fileRF='randomforestall.ml'
@@ -32,25 +26,10 @@ if __name__ == "__main__":
 
         clf = ensemble.RandomForestClassifier(n_estimators=noTree,criterion=critF)
         print("Classifier:\n\t %s"%str(clf))
-        #x_train, x_test, y_train, y_test = cross_validation.train_test_split(traindata, target, test_size=0.2, random_state=1)
-        #clf=clf.fit(train,target)
-        #print(clf.score(x_test,y_test))
-        #streamobj.saveMe(clf,fileRF)
         clf=streamobj.loadMe(fileRF)
         print(test.shape)
         rfresult=clf.predict(test)
         print(rfresult.shape)
-        #result = [[0 for x in range(28001)] for x in range(2)] 
-
-        #result[0][0]='Label'
-        #result[0][1]='ImageId'
-        #result[1:28001][0]=range(1,28001)
-        #result[1:28001][1]=rfresult[0:28000]
-        #result=np.ones((28000,2))  
-        #result[:,0]=np.arange(0,28000)
-        #result[:,1]=rfresult[:]
         df = pd.DataFrame(rfresult)
         df.index+=1
         df.to_csv('resultmnst.csv')
-        #df.to_csv('resultmnst.csv',index=True,index_label=range(2,28001))
-        #np.savetxt("resultmnst.csv", result, delimiter=",",fmt='%1d')
